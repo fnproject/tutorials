f8ad9900e74cf8b6d1fcac7fd7f67610d690d4c4
Thom Leggett <thom@tteggel.org>
2017-11-28 18:59:04 +0000

    FlowSaga updates get reverted. Verting.

--- FlowSaga/scripts/pull-latest.sh
+++ FlowSaga/scripts/pull-latest.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-docker pull fnproject/functions:latest
+docker pull fnproject/fnserver:latest
 docker pull fnproject/flow:latest
 docker pull fnproject/flow:ui
 docker pull fnproject/fn-java-fdk-build:latest

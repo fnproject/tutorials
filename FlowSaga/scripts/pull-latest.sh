090052abb3c472b535a1141e10b21639b417cb4e
Owen Cliffe <owen.cliffe@oracle.com>
2017-12-13 15:53:25 +0000

    rev fdk add imag to func.yaml

--- FlowSaga/scripts/pull-latest.sh
+++ FlowSaga/scripts/pull-latest.sh
@@ -3,7 +3,3 @@
 docker pull fnproject/fnserver:latest
 docker pull fnproject/flow:latest
 docker pull fnproject/flow:ui
-docker pull fnproject/fn-java-fdk-build:latest
-docker pull fnproject/fn-java-fdk:latest
-docker pull fnproject/fn-java-fdk:jdk9-latest
-docker pull fnproject/fn-java-fdk-build:jdk9-latest

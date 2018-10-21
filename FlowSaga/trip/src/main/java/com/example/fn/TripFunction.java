3bee2d4e23797282ba6738964aa873c8e023d041
David Delabassee <david.delabassee@gmail.com>
2018-10-21 03:54:54 -0700

    Update trip func

--- FlowSaga/trip/src/main/java/com/example/fn/TripFunction.java
+++ FlowSaga/trip/src/main/java/com/example/fn/TripFunction.java
@@ -1,4 +1,4 @@
-package com.example.fn;
+	package com.example.fn;
 
 import com.example.fn.messages.BookingRes;
 import com.example.fn.messages.EmailReq;
@@ -6,91 +6,121 @@ import com.example.fn.messages.TripReq;
 import com.fnproject.fn.api.flow.Flow;
 import com.fnproject.fn.api.flow.FlowFuture;
 import com.fnproject.fn.api.flow.Flows;
+import com.fnproject.fn.api.FnFeature;
+import com.fnproject.fn.runtime.flow.FlowFeature;
+
+import com.fnproject.fn.api.FnConfiguration;
+import com.fnproject.fn.api.RuntimeContext;
 
 import java.io.Serializable;
 
+@FnFeature(FlowFeature.class)
 public class TripFunction implements Serializable {
 
-    public void book1(TripReq input) {
+    String funcFlightBook;
+    String funcFlightCancel;
+    String funcHotelBook;
+    String funcHotelCancel;
+    String funcCarBook;
+    String funcCarCancel;
+    String funcEmail;
+
+    @FnConfiguration
+    public void configure(RuntimeContext ctx) {
+        funcFlightBook = ctx.getConfigurationByKey("FLIGHT-BOOK-ID")
+                .orElseThrow(() -> new RuntimeException("Missing FunctionId"));
+
+        funcFlightCancel = ctx.getConfigurationByKey("FLIGHT-CANCEL-ID")
+                .orElseThrow(() -> new RuntimeException("Missing FunctionId"));
+
+        funcHotelBook = ctx.getConfigurationByKey("HOTEL-BOOK-ID")
+                .orElseThrow(() -> new RuntimeException("Missing FunctionId"));
+
+        funcHotelCancel = ctx.getConfigurationByKey("HOTEL-CANCEL-ID")
+                .orElseThrow(() -> new RuntimeException("Missing FunctionId"));
+
+        funcCarBook = ctx.getConfigurationByKey("CAR-BOOK-ID")
+                .orElseThrow(() -> new RuntimeException("Missing FunctionId"));
+
+        funcCarCancel = ctx.getConfigurationByKey("CAR-CANCEL-ID")
+                .orElseThrow(() -> new RuntimeException("Missing FunctionId"));
+
+        funcEmail = ctx.getConfigurationByKey("EMAIL-ID")
+                .orElseThrow(() -> new RuntimeException("Missing FunctionId"));
+
+        EmailReq.setFuncEmail(funcEmail); // todo remove - set email functionId
+
+    }
+
+    public void bookFlightHotel(TripReq input) {
+
         Flow f = Flows.currentFlow();
 
         FlowFuture<BookingRes> flightFuture =
-            f.invokeFunction("./flight/book", input.flight, BookingRes.class);
+                f.invokeFunction(funcFlightBook, input.flight, BookingRes.class);
 
         FlowFuture<BookingRes> hotelFuture =
-            f.invokeFunction("./hotel/book", input.hotel, BookingRes.class);
-
-        FlowFuture<BookingRes> carFuture =
-            f.invokeFunction("./car/book", input.carRental, BookingRes.class);
+                f.invokeFunction(funcHotelBook, input.hotel, BookingRes.class);
 
         flightFuture.thenCompose(
-            (flightRes) -> hotelFuture.thenCompose(
-                (hotelRes) -> carFuture.whenComplete(
-                    (carRes, e) -> EmailReq.sendSuccessMail(flightRes, hotelRes, carRes)
+                (flightRes) -> hotelFuture.whenComplete(
+                        (hotelRes, e) -> EmailReq.sendSuccessMail(flightRes, hotelRes)
                 )
-            )
         );
+
     }
 
 
+    public void bookAll(TripReq input) {
 
-    public void book2(TripReq input) {
         Flow f = Flows.currentFlow();
 
         FlowFuture<BookingRes> flightFuture =
-            f.invokeFunction("./flight/book", input.flight, BookingRes.class);
+                f.invokeFunction(funcFlightBook, input.flight, BookingRes.class);
 
         FlowFuture<BookingRes> hotelFuture =
-            f.invokeFunction("./hotel/book", input.hotel, BookingRes.class);
+                f.invokeFunction(funcHotelBook, input.hotel, BookingRes.class);
 
         FlowFuture<BookingRes> carFuture =
-            f.invokeFunction("./car/book", input.carRental, BookingRes.class);
+                f.invokeFunction(funcCarBook, input.carRental, BookingRes.class);
+
 
         flightFuture.thenCompose(
-            (flightRes) -> hotelFuture.thenCompose(
-                (hotelRes) -> carFuture.whenComplete(
-                    (carRes, e) -> EmailReq.sendSuccessMail(flightRes, hotelRes, carRes)
-                )
-                .exceptionallyCompose( (e) -> cancel("./car/cancel", input.carRental, e) )
-            )
-            .exceptionallyCompose( (e) -> cancel("./hotel/cancel", input.hotel, e) )
-        )
-        .exceptionallyCompose( (e) -> cancel("./flight/cancel", input.flight, e) )
-        .exceptionally( (err) -> {EmailReq.sendFailEmail(); return null;} );
+                (flightRes) -> hotelFuture.thenCompose(
+                        (hotelRes) -> carFuture.whenComplete(
+                                (carRes, e) -> EmailReq.sendSuccessMail(flightRes, hotelRes, carRes)
+                        ).exceptionallyCompose( (e) -> cancel(funcCarCancel, input.carRental, e) )
+                ).exceptionallyCompose( (e) -> cancel(funcHotelCancel, input.hotel, e) )
+        ).exceptionallyCompose( (e) -> cancel(funcFlightCancel, input.flight, e) )
+        .exceptionally( (err) -> {
+                                EmailReq.sendFailEmail();
+                                return null;
+                    } );
+        ;
+
     }
+
+    public void bookHotelOnly(TripReq input) {
+
+        Flow f = Flows.currentFlow();
+
+        FlowFuture<BookingRes> flightFuture =
+                f.invokeFunction(funcFlightBook, input.flight, BookingRes.class);
+
+        flightFuture.whenComplete((bookingRes, throwable) -> {
+            if (throwable != null) {
+                System.out.println(" throwable : " + throwable.getMessage());
+            } else EmailReq.sendSuccessMail(bookingRes, bookingRes);
+        });
+    }
+
 
     private static FlowFuture<BookingRes> cancel(String cancelFn, Object input, Throwable e) {
         Flows.currentFlow().invokeFunction(cancelFn, input, BookingRes.class);
         return Flows.currentFlow().failedFuture(e);
     }
 
-
-
-    public void book3(TripReq input) {
-        Flow f = Flows.currentFlow();
-
-        FlowFuture<BookingRes> flightFuture =
-            f.invokeFunction("./flight/book", input.flight, BookingRes.class);
-
-        FlowFuture<BookingRes> hotelFuture =
-            f.invokeFunction("./hotel/book", input.hotel, BookingRes.class);
-
-        FlowFuture<BookingRes> carFuture =
-            f.invokeFunction("./car/book", input.carRental, BookingRes.class);
-
-        flightFuture.thenCompose(
-            (flightRes) -> hotelFuture.thenCompose(
-                (hotelRes) -> carFuture.whenComplete(
-                    (carRes, e) -> EmailReq.sendSuccessMail(flightRes, hotelRes, carRes)
-                )
-                .exceptionallyCompose( (e) -> retryCancel("./car/cancel", input.carRental, e) )
-            )
-            .exceptionallyCompose( (e) -> retryCancel("./hotel/cancel", input.hotel, e) )
-        )
-        .exceptionallyCompose( (e) -> retryCancel("./flight/cancel", input.flight, e) )
-        .exceptionally( (err) -> {EmailReq.sendFailEmail(); return null;} );
-    }
-
+ 
     private static FlowFuture<BookingRes> retryCancel(String cancelFn, Object input, Throwable e) {
         Retry.exponentialWithJitter(
             () -> Flows.currentFlow().invokeFunction(cancelFn, input, BookingRes.class));
--- FlowSaga/trip/src/main/java/com/example/fn/messages/EmailReq.java
+++ FlowSaga/trip/src/main/java/com/example/fn/messages/EmailReq.java
@@ -1,5 +1,7 @@
 package com.example.fn.messages;
 
+import com.fnproject.fn.api.FnConfiguration;
+import com.fnproject.fn.api.RuntimeContext;
 import com.fnproject.fn.api.flow.Flows;
 
 import java.io.Serializable;
@@ -7,9 +9,29 @@ import java.io.Serializable;
 public class EmailReq implements Serializable {
     public String message;
 
+    public static void setFuncEmail(String funcEmail) {
+        EmailReq.funcEmail = funcEmail;
+    }
+
+    static private String funcEmail;
+
+    public static void sendFailEmail() {
+        System.out.println("----> sendFailEmail : " + funcEmail);
+        Flows.currentFlow().invokeFunction(funcEmail, composeFailEmail());
+    }
+
+    public static void sendSuccessMail(BookingRes flightRes, BookingRes hotelRes, BookingRes carRes) {
+        Flows.currentFlow().invokeFunction(funcEmail, composeSuccessEmail(flightRes, hotelRes, carRes));
+    }
+
+    public static void sendSuccessMail(BookingRes flightRes, BookingRes carRes) {
+        Flows.currentFlow().invokeFunction(funcEmail, composeSuccessEmail(flightRes, carRes));
+    }
+
     public static EmailReq composeSuccessEmail(BookingRes flightResponse,
                                                BookingRes hotelResponse,
                                                BookingRes carResponse) {
+
         EmailReq result = new EmailReq();
         result.message = "Flight confirmation: " + flightResponse.confirmation + "\n" +
                 "Hotel confirmation: " +  hotelResponse.confirmation + "\n" +
@@ -17,18 +39,25 @@ public class EmailReq implements Serializable {
         return result;
     }
 
+    public static EmailReq composeSuccessEmail(BookingRes flightResponse,
+                                               BookingRes hotelResponse) {
+        EmailReq result = new EmailReq();
+        result.message = "Flight confirmation: " + flightResponse.confirmation + "\n" +
+                "Hotel confirmation: " +  hotelResponse.confirmation ;
+        return result;
+    }
+
+
+
     public static EmailReq composeFailEmail() {
         EmailReq result = new EmailReq();
         result.message = "We failed to book your trip, sorry.";
         return result;
     }
 
-    public static void sendFailEmail() {
-        Flows.currentFlow().invokeFunction("./email", composeFailEmail());
-    }
+/*    public static void sendSuccessMail(BookingRes flightRes, BookingRes hotelRes) {
+        EmailReq message = composeSuccessEmail(flightRes, hotelRes);
+        Flows.currentFlow().invokeFunction(funcEmail, message);
+    }*/
 
-    public static void sendSuccessMail(BookingRes flightRes, BookingRes hotelRes, BookingRes carRes) {
-        EmailReq message = composeSuccessEmail(flightRes, hotelRes, carRes);
-        Flows.currentFlow().invokeFunction("./email", message);
-    }
 }

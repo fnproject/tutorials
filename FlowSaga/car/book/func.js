0ba831b3b62038b499aad27d9d76af485f9f7fce
David Delabassee <david.delabassee@gmail.com>
2019-01-28 16:56:53 +0100

    fix for Node car book & cancel functions

--- FlowSaga/car/book/func.js
+++ FlowSaga/car/book/func.js
@@ -1,3 +1,38 @@
+const fdk=require('@fnproject/fdk');
+const request = require('request');
+
+function initializePromise() {
+    var options = {
+        uri: process.env.CAR_API_URL
+    };
+    return new Promise(function(resolve, reject) {
+        request.post(options, function(err, resp, body) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(body));
+            }
+        })
+    })
+
+}
+
+
+fdk.handle(function(input){
+
+   return initializePromise().then(function(result) {
+      return result;
+   }, function(err) {
+      console.log(err);
+      return (JSON.stringify({ "status": "remote car-book error!"}))
+   })
+
+})
+
+
+
+
+/*
 var request = require('request');
 var fs = require('fs');
 
@@ -15,3 +50,5 @@ request.post(
         }
     }
 );
+
+*/
--- FlowSaga/car/cancel/func.js
+++ FlowSaga/car/cancel/func.js
@@ -1,3 +1,38 @@
+const fdk=require('@fnproject/fdk');
+const request = require('request');
+
+function initializePromise() {
+    var options = {
+		  uri: process.env.CAR_API_URL
+    };
+    return new Promise(function(resolve, reject) {
+        request.delete(options, function(err, resp, body) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(body));
+            }
+        })
+    })
+
+}
+
+
+fdk.handle(function(input){
+
+   return initializePromise().then(function(result) {
+      return result;
+   }, function(err) {
+      console.log(err);
+      return (JSON.stringify({ "status": "remote car-book error!"}))
+   })
+
+})
+
+
+
+
+/*
 var request = require('request');
 
 var api_url = process.env.CAR_API_URL;
@@ -12,4 +47,4 @@ request.delete(
             throw new Error();
         }
     }
-);
+);*/

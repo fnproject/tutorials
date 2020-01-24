469499ab6fa712439b54ccb74a9118a7f5761f64
Michael Williams <michael.w.williams@oracle.com>
2020-01-24 11:34:21 -0700

    Formatting Tweaks

--- basics/UsingRuntimeContext/code/java/cfg-fn/src/main/java/com/example/fn/HelloFunction.java
+++ basics/UsingRuntimeContext/code/java/cfg-fn/src/main/java/com/example/fn/HelloFunction.java
@@ -3,33 +3,33 @@ package com.example.fn;
 import com.fnproject.fn.api.FnConfiguration;
 import com.fnproject.fn.api.RuntimeContext;
 
-
 public class HelloFunction {
-	/* Vars for Env Variables */
-	private String dbHost;		// DB_HOST
-	private String dbUser;		// DB_USER
-	private String dbPassword;	// DB_PASSWORD
-	
-	@FnConfiguration
-	public void config(RuntimeContext ctx) {
-		
-		dbHost = ctx.getConfigurationByKey("DB_HOST_URL").orElse("//localhost/DBName");
-		
-		dbUser = ctx.getConfigurationByKey("DB_USER").orElse("your-db-account");
-				
-		dbPassword = ctx.getConfigurationByKey("DB_PASSWORD").orElse("your-db-password");
-		
-	}
-	
+
+    /* Vars for Env Variables */
+    private String dbHost;		// DB_HOST
+    private String dbUser;		// DB_USER
+    private String dbPassword;	// DB_PASSWORD
+
+    @FnConfiguration
+    public void config(RuntimeContext ctx) {
+
+        dbHost = ctx.getConfigurationByKey("DB_HOST_URL").orElse("//localhost/DBName");
+
+        dbUser = ctx.getConfigurationByKey("DB_USER").orElse("your-db-account");
+
+        dbPassword = ctx.getConfigurationByKey("DB_PASSWORD").orElse("your-db-password");
+
+    }
+
     public String handleRequest(String input) {
-    	String resultStr = "";
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
+        String resultStr = "";
+        String name = (input == null || input.isEmpty()) ? "world" : input;
 
         resultStr = resultStr + "Hello " + name + "!\n";
         resultStr = resultStr + "DB Host URL: " + dbHost + "\n";
         resultStr = resultStr + "DB User: " + dbUser + "\n";
         resultStr = resultStr + "DB Passwd: " + dbPassword + "\n";
-        
+
         return resultStr;
     }
 
